4a0303111251b6e4fdabeddf7ce8f54eae1e4079
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2023-02-03 13:39:44 +0530

    Navbar...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import Home from './routes/Home';
+import About from './routes/About';
+import Project from './routes/Project';
+import Contact from './routes/Contact';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+    <Routes>
+      <Route path ='/' element={<Home />} />
+      <Route path ='/Project' element={<Project />} />
+      <Route path ='/about' element={<About />} />
+      <Route path ='/Contact' element={<Contact />} />
+    </Routes>
+    </>
+    
   );
 }
 
